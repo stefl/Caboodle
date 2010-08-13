5c0803690e228dd62b47e50a235c930a225c795f
steflewandowski <stef@stef.io>
2010-08-13 09:25:22 +0100

    clean up config loading

--- lib/caboodle/config.rb
+++ lib/caboodle/config.rb
@@ -1,11 +1,11 @@
-module Caboodle
-  MenuItems = []
-  Defaults = Hashie::Mash.new()
-  Site = Defaults.clone
-  RequiredSettings = Hashie::Mash.new()
+module Caboodle  
   Kits = []
+  MenuItems = []
   Javascripts = []
   Stylesheets = []
+  Defaults = Hashie::Mash.new()
+  RequiredSettings = Hashie::Mash.new()
+  Site = Defaults.clone
   Site.required_settings = []
   Site.kits = [] unless Site.kits
   Defaults.merge!(Hashie::Mash.new(YAML.load_file(File.join( File.join(File.dirname(__FILE__), 'config','defaults.yml')))))
