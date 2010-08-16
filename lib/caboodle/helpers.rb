d0231544f9fc9618bcb5efef83bc68f23778ab29
steflewandowski <stef@stef.io>
2010-08-16 06:20:43 +0100

    page titles and analytics

--- lib/caboodle/helpers.rb
+++ lib/caboodle/helpers.rb
@@ -9,6 +9,15 @@ module Sinatra
       #Caboodle::Kit.credit
     end
     
+    def title
+      if request.path_info == "/"
+        "#{Caboodle::Site.title} | #{Caboodle::Site.description}"
+      else
+        t = Caboodle::Site.title
+        t = "#{@title} | #{t}" if @title
+      end
+    end
+    
   end
   helpers CaboodleHelpers
 end
--- lib/caboodle/kits/analytics/analytics.rb
+++ lib/caboodle/kits/analytics/analytics.rb
@@ -1,14 +1,19 @@
 module Caboodle
   class Analytics < Caboodle::Kit
     add_to_layout "after_body" => "<script type=\"text/javascript\">
-    	var gaJsHost = ((\"https:\" == document.location.protocol) ? \"https://ssl.\" : \"http://www.\");
-    	document.write(unescape(\"%3Cscript src='\" + gaJsHost + \"google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E\"));
-    </script>
-    <script type=\"text/javascript\">
-    	try {
-    	var pageTracker = _gat._getTracker(\"#{Caboodle::Site.analytics_id}\");
-    	pageTracker._trackPageview();
-    	} catch(err) {}
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '#{Caboodle::Site.analytics_id}']);
+      _gaq.push(['_setDomainName', 'none']);
+      _gaq.push(['_setAllowLinker', true]);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
     </script>"
     required [:analytics_id]
   end
