e63a1481e0b3a109d56381b2bccd411e1734dea3
steflewandowski <stef@stef.io>
2010-08-24 17:30:13 +0100

    if there is any weirdness in the home_kit setting bail to the standard home page

--- lib/caboodle/kits/standard/standard.rb
+++ lib/caboodle/kits/standard/standard.rb
@@ -1,8 +1,9 @@
 module Caboodle
   class Standard < Caboodle::Kit
     required [:title, :description, :logo_url, :author]
-    
-    if Caboodle::Site.home_kit.to_s.blank?   
+    puts "Test home kit"
+    if Caboodle::Site.home_kit.blank? || !(Caboodle::Kit.available_kits.include?(Caboodle::Site.home_kit)) || !(Caboodle::Kits.include?(Caboodle::Site.home_kit))
+      puts "There is no home page kit"
       get "/" do
         haml :standard
       end
