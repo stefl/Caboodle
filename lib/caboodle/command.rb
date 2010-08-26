b0538acb4dd758a28463239ac0a22451834d412a
steflewandowski <stef@stef.io>
2010-08-26 16:52:17 +0800

    kit:list on the command line. Remove heroku dependency in the gem

--- lib/caboodle/command.rb
+++ lib/caboodle/command.rb
@@ -1,22 +1,18 @@
-gem 'heroku'
-require 'heroku'
-require 'heroku/command'
-
 module Caboodle
   module Command
     class << self
       def run(command, args, retries=0)
         case command
         when "create"
+        
           puts `mkdir #{args.first}`
           puts `cd #{args.first} && cp -r #{File.expand_path(File.join(File.dirname(__FILE__), 'app'))}/* .`
           puts `cd #{args.first} && cp #{File.expand_path(File.join(File.dirname(__FILE__), 'app'))}/.gems .`
           puts `cd #{args.first} && git init`
+          Caboode::Kit.ask_user
           Caboodle::Kit.configure
           puts `cd #{args.first} && git add .`
           puts `cd #{args.first} && git commit -m"initial setup"`
-          puts `cd #{args.first} && heroku create #{args.first}`
-          
         when /kit:add/
           Caboodle::Kit.load_kit args.first.capitalize
           puts "Dump config"
@@ -29,7 +25,12 @@ module Caboodle
         when /kit:home/
           if Caboodle::Kit.available_kits.include?(args.first.capitalize)
             Caboodle::Site.home_kit = args.first.capitalize
+          else
+            puts "Sorry - that Kit isn't available. Try:"
+            Caboodle::Kit.available_kits.each {|kit| puts kit}
           end
+        when /kit:list/
+          Caboodle::Kit.available_kits.each {|kit| puts kit}
         when /kit:remove/
           Caboodle::Kit.unload_kit args.first.capitalize
           puts "Pushing to Heroku"
@@ -38,10 +39,14 @@ module Caboodle
           puts `git push heroku master`
           puts "Done!"
         when "deploy"
+          gem 'heroku'
+          require 'heroku'
+          require 'heroku/command'
+        
           puts `git commit -m"deploy" -a`
           puts `git push heroku master`
         else
-          Heroku::Command.run(command,args,retries)
+          puts "Sorry, that command is not recognized"
         end
       end
     end
--- lib/caboodle/scrape.rb
+++ lib/caboodle/scrape.rb
@@ -1,9 +1,12 @@
+require 'nokogiri'
+require 'hashie'
+
 module Caboodle
   def self.scrape url
-    Nokogiri::HTML(Weary.get(url).perform_sleepily.body)
+    ::Nokogiri::HTML(Weary.get(url).perform_sleepily.body)
   end
   
   def self.mash req
-    Hashie::Mash.new(req.perform_sleepily.parse)
+    ::Hashie::Mash.new(req.perform_sleepily.parse)
   end
 end
