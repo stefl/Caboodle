a88eb0138fc2629726411678125772ca98d6878d
steflewandowski <stef@stef.io>
2010-08-27 12:50:25 +0800

    command line options for getting and setting configuration

--- lib/caboodle/app.rb
+++ lib/caboodle/app.rb
@@ -10,7 +10,7 @@ module Caboodle
     helpers Sinatra::CaboodleHelpers
     
     configure do
-      Caboodle::Kit.configure File.expand_path(File.join(Caboodle::App.root,"config","site.yml"))
+      Caboodle::Kit.configure_site File.expand_path(File.join(Caboodle::App.root,"config","site.yml"))
     end
   
   end
