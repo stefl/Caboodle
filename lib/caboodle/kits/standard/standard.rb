c2a97847b34ac1cff59295196257cbba48626be1
steflewandowski <stef@stef.io>
2010-08-27 13:05:49 +0800

    more helpful help

--- lib/caboodle/kits/standard/standard.rb
+++ lib/caboodle/kits/standard/standard.rb
@@ -2,7 +2,7 @@ module Caboodle
   class Standard < Caboodle::Kit
     required [:title, :description, :author]
     optional [:logo_url]
-    if Caboodle::Site.home_kit.blank? || !(Caboodle::Kit.available_kits.include?(Caboodle::Site.home_kit)) || !(Caboodle::Kits.include?(Caboodle::Site.home_kit))
+    if Caboodle::Site.home_kit.blank?
       puts "Using the default home kit - you will want to run 'caboodle kit:home <kit name>' to specify your own kit"
       get "/" do
         haml :standard
