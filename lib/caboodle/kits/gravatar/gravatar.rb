4851be246562ced7adc8ee1a7d199ae1b01fa424
steflewandowski <stef@stef.io>
2010-08-27 12:54:37 +0800

    remove heroku-dependent stuff. You should be able to use whatever you like to deploy.

--- lib/caboodle/kits/gravatar/gravatar.rb
+++ lib/caboodle/kits/gravatar/gravatar.rb
@@ -2,10 +2,10 @@ require 'digest/md5'
 
 module Caboodle
   class Standard < Caboodle::Kit
-    required [:email]
+    optional [:email]
     
     configure do
-      unless Caboodle::Site.logo_url
+      if Caboodle::Site.logo_url.to_s.blank? && !Caboodle::Site.email.to_s.blank?
         hash = Digest::MD5.hexdigest(Caboodle::Site.email)
         Caboodle::Site.logo_url = "http://www.gravatar.com/avatar/#{hash}"
       end
