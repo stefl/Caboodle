3696ba615dd8ea7f19b60476e645f5540afa9c3e
steflewandowski <stef@stef.io>
2010-08-31 16:18:15 +0800

    Susy fixes for access link. Google Local config. Rescue SocketErrors in production.

--- lib/caboodle/kits/googlelocal/googlelocal.rb
+++ lib/caboodle/kits/googlelocal/googlelocal.rb
@@ -9,11 +9,30 @@ module Caboodle
     
     stylesheets ["http://www.google.com/uds/solutions/mapsearch/gsmapsearch.css", "http://www.google.com/uds/css/gsearch.css"]
     
+    before do
+      @locations = Config.google_local_locations
+    end
+    
     menu "Near me", "/near_me" do
       @title = "Near me"
-      @locations = Config.google_local_locations
-      puts @locations.inspect
+      @location = Config.google_local_locations.first
       haml :googlelocal
+    end
+    
+    get "/near_me/:slug" do
+      Config.google_local_locations.each do |loc|
+        if loc.title.downcase.gsub(" ","-") == params[:slug]
+          @location = loc
+        end
+      end
+      
+      puts @location.inspect
+      
+      if @location
+        haml :googlelocal
+      else
+        redirect "/near_me" 
+      end
     end
     
     add_sass ["googlelocal"]
