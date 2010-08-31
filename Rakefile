cae76a5c06e09b9a8daf65d9bc993dc4af909d87
steflewandowski <stef@stef.io>
2010-08-31 10:24:49 +0100

    version bump

--- Rakefile
+++ Rakefile
@@ -22,6 +22,7 @@ begin
     gem.add_dependency "sleepy",">=0.1.4"
     gem.add_dependency "tumblr-rb"
     gem.add_dependency "rack-geo"
+    gem.add_dependency "maruku"
   end
   Jeweler::GemcutterTasks.new
 rescue LoadError
--- lib/caboodle/kits/about/about.rb
+++ lib/caboodle/kits/about/about.rb
@@ -6,13 +6,12 @@ module Caboodle
     description "Gives you an About page at /about. Edit the Markdown file located in your application config directory."
     
     files ["about.md"]
-    
-    config_files ["about.yml"]
-    
+        
     menu "About", "/about" do
+      @title = "About"
       md = File.expand_path(File.join(Caboodle::App.root,"config","about.md"))
       @content = Maruku.new(open(md).read).to_html_document
-      haml ".page.about= @content"
+      haml ".page.about.thin_page= @content"
     end
   end
 end
