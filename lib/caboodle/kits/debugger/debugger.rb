d2cd14016be2638b3150ec662e6525ba7634beeb
steflewandowski <stef@stef.io>
2010-08-31 16:32:18 +0800

    Using Caboodle::Kit::Config over Caboodle::Site for complex config

--- lib/caboodle/kits/debugger/debugger.rb
+++ lib/caboodle/kits/debugger/debugger.rb
@@ -1,8 +1,6 @@
 module Caboodle
   class Debugger < Caboodle::Kit
     
-    puts "Running debugger"
-    puts Caboodle::Debug.inspect
     Caboodle::Debug.each do |debug|
       Caboodle::Layout.below_footer = "<div id='debug'>"
       Caboodle::Layout.below_footer << "<p>#{debug}</p>"
--- lib/caboodle/kits/history/history.rb
+++ lib/caboodle/kits/history/history.rb
@@ -13,18 +13,9 @@ require "sinatra/advanced_routes"
 module Caboodle
   class History < Caboodle::Kit
       
-    description "By editing a YAML file you can list things that you have done in the past."
-    config_files ["history.yml"]
+    description "A list of things you have done in the past."
     
-    configure do
-      config_path = File.expand_path(File.join(Caboodle::App.root,"config","history.yml"))
-      if File.exists?(config_path)
-        Caboodle::Kit.load_config(config_path)
-      else
-        `cp "#{File.join(File.dirname(__FILE__),"config","history.yml")}" "#{File.join(Caboodle::App.root,"config",".")}"` rescue "Could not create the sample yml file"
-        puts "Please enter your history items in the file #{File.expand_path(File.join(Caboodle::App.root,"config","history.yml"))}"
-      end
-    end
+    config_files ["history.yml"]
         
     menu "History", "/history" do
       @title = "History"
