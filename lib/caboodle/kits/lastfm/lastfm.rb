c32aa6187b9ecf42047a642f7be4162b7c29dd1a
steflewandowski <stef@stef.io>
2010-09-02 07:59:38 +0100

    Last.fm kit

---
+++ lib/caboodle/kits/lastfm/lastfm.rb
@@ -0,0 +1,23 @@
+
+class Caboodle::Lastfm < Caboodle::Kit
+  description "Displays your recent and all time top Last.fm plays"
+  
+  required [:lastfm_username]
+  
+  def scrape 
+    @last ||= Caboodle.scrape("http://www.last.fm/user/#{Caboodle::Site.lastfm_username}")
+  end
+  
+  menu "Listening" do
+
+    @recent = scrape.css("#recentTracks .module-body").to_html
+    @recent.gsub!("href=\"/", 'href="http://last.fm/')
+    @top = scrape.css(".modulechartsartists .module-body").to_html
+    @top.gsub!("href=\"/", 'href="http://last.fm/')
+    
+    haml :listening
+  end
+  
+  credit "http://www.last.fm/listen/user/#{Caboodle::Site.lastfm_username}", "#{Caboodle::Site.lastfm_username} on Last.fm"
+  
+end
